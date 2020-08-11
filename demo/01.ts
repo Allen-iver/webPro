c6570bf055116f0cacd19802582fa8e15990ec00
guochuan <guochuan@juren.com>
2020-08-11 16:25:07 +0800

    代碼合併

--- demo/01.ts
+++ demo/01.ts
@@ -4,3 +4,24 @@ enum exapmle {
   No = 0,
   Yes = 'YES'
 }
+
+/* 声明 */
+interface IA {
+  id: string
+}
+type TA = {
+  id: string
+}
+/* 继承 */
+interface IA2 extends IA {
+    name: string
+}
+type TA2 = TA & { name: string }
+/* 实现 */
+class A implements IA {
+    id: string = ''
+}
+class A2 implements TA {
+    id: string = ''
+}
+
